8a59b5511a9fe33535d7cd6c65bd2d9dbee10e19
Munsat Rukaya <munsatrukaya@192-168-1-101.tpgi.com.au>
2023-03-04 18:22:27 +1100

    added search capabilities in upcoming events

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,14 @@
-const homeEvent = document.querySelector('.home-events')
+function searchFunc() {
+  const searchInput = document.querySelector(".search-input");
+  const allNames = document.querySelectorAll(".event-name");
+  const eventContainer = document.querySelectorAll(".upcoming-events");
+  const filterValue = searchInput.value.toLowerCase();
+
+  for (i = 0; i < allNames.length; i++) {
+    if (allNames[i].textContent.toLowerCase().indexOf(filterValue) > -1) {
+      eventContainer[i].style.display = "";
+    } else {
+      eventContainer[i].style.display = "none";
+    }
+  }
+}
